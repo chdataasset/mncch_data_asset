7f01ef0a0dd820b01645c9d80af5f4f2e65f32c6
rahadians <matrix.black82@gmail.com>
2022-05-23 18:47:37 +0700

    pengerjaan home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -22,54 +22,85 @@ class HomeView extends GetView<HomeController> {
             );
           }
 
-          return Obx(
-            () => (controller.allList.length == null)
-                ? Center(
-                    child: Text(
-                      "Data Tidak Ada",
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  )
-                : RefreshIndicator(
-                    onRefresh: () => controller.getAllList(),
-                    child: (controller.isLoading.value)
-                        ? CircularProgressIndicator()
-                        // : Text("loading")
-                        : Center(
-                            child: GridView.builder(
-                              itemCount: controller.allList.length,
-                              gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                crossAxisSpacing: 2,
-                                mainAxisSpacing: 2,
-                                // mainAxisExtent: 5,
-                                childAspectRatio: 3 / 4,
-                              ),
-                              itemBuilder: (context, index) {
-                                TableListHome listBody =
-                                    controller.allList[index];
-
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: LayoutBuilder(
-                                    builder: ((context, constraints) {
-                                      return Card(
-                                        child: Column(
-                                          children: [
-                                            Icon(listBody.icon.to),
-                                            Text("${listBody.judul}"),
-                                          ],
-                                        ),
-                                      );
-                                    }),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
+          return Obx(() => (controller.allList.length == null)
+              ? Center(
+                  child: Text(
+                    "Data Tidak Ada",
+                    style: TextStyle(fontSize: 16),
                   ),
-          );
+                )
+              : RefreshIndicator(
+                  onRefresh: () => controller.getAllList(),
+                  child: (controller.isLoading.value)
+                      ? CircularProgressIndicator()
+                      // : Text("loading")
+                      : SafeArea(
+                          child: Center(
+                            child:
+                                LayoutBuilder(builder: (context, constraints) {
+                              final width = constraints.maxWidth;
+                              final height = constraints.maxHeight;
+                              return Column(
+                                children: [
+                                  Container(
+                                    alignment: Alignment.topCenter,
+                                    height: 0.25,
+                                    width: width,
+                                    color: Colors.red,
+                                  ),
+                                  Container(
+                                    width: width,
+                                    height: height * 0.50,
+                                    color: Colors.amber,
+                                    child: GridView.builder(
+                                      itemCount: controller.allList.length,
+                                      gridDelegate:
+                                          SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 3,
+                                        crossAxisSpacing: 2,
+                                        mainAxisSpacing: 2,
+                                        // mainAxisExtent: 5,
+                                        childAspectRatio: 1 / 1,
+                                      ),
+                                      itemBuilder: (context, index) {
+                                        //   Text("${listBody.judul}"),
+                                        TableListHome listBody =
+                                            controller.allList[index];
+                                        return Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: LayoutBuilder(
+                                            builder: ((context, constraints) {
+                                              return Column(
+                                                children: [
+                                                  Container(
+                                                    width: constraints.maxWidth,
+                                                    height:
+                                                        constraints.maxHeight *
+                                                            0.85,
+                                                    decoration: BoxDecoration(
+                                                      image:
+                                                          new DecorationImage(
+                                                        image: AssetImage(
+                                                            "${listBody.icon}"),
+                                                        fit: BoxFit.fill,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  // Text("${listBody.judul}"),
+                                                ],
+                                              );
+                                            }),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  )
+                                ],
+                              );
+                            }),
+                          ),
+                        ),
+                ));
         },
       ),
     );
